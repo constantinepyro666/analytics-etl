c0ec96323711646c60f9b51912a89dc619e8fe38
constantinepyro666 <constantine.pyro666@gmail.com>
2026-04-17 10:57:12 +0500

    Update daily_metrics.sql

--- sql/daily_metrics.sql
+++ sql/daily_metrics.sql
@@ -1,1 +1,10 @@
+DROP TABLE IF EXISTS daily_metrics;
 
+CREATE TABLE daily_metrics AS
+SELECT
+    DATE(event_time) AS date,
+    platform,
+    COUNT(DISTINCT user_id) AS dau
+FROM sessions
+GROUP BY date, platform
+ORDER BY date, platform;

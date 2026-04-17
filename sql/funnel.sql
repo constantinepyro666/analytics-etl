b787e901fce7623a11c0ca1184d491ddd9406673
constantinepyro666 <constantine.pyro666@gmail.com>
2026-04-17 13:46:58 +0500

    Update funnel.sql

--- sql/funnel.sql
+++ sql/funnel.sql
@@ -1,1 +1,14 @@
+DROP TABLE IF EXISTS funnel;
 
+CREATE TABLE funnel AS
+SELECT
+    platform,
+
+    COUNT(DISTINCT CASE WHEN event_type = 'signup' THEN user_id END) AS signup,
+
+    COUNT(DISTINCT CASE WHEN event_type = 'view_item' THEN user_id END) AS view_item,
+
+    COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) AS purchase
+
+FROM sessions
+GROUP BY platform;

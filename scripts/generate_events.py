936483f44087eeef90d7a67b6f298fecee54f56b
constantinepyro666 <constantine.pyro666@gmail.com>
2026-04-23 15:57:20 +0500

    Update generate_events.py

--- scripts/generate_events.py
+++ scripts/generate_events.py
@@ -19,15 +19,12 @@ EVENTS_PER_RUN = 3000
 
 platforms = ["iOS", "Android", "Web"]
 
-now = datetime.now()
-
 # фиксированный пул пользователей (для retention)
 users = list(range(1, NUM_USERS + 1))
 
 for _ in range(EVENTS_PER_RUN):
     user_id = random.choice(users)
 
-    # распределение событий (похоже на реальность)
     event_type = random.choices(
         ["signup", "view_item", "purchase"],
         weights=[0.2, 0.6, 0.2]
@@ -35,13 +32,14 @@ for _ in range(EVENTS_PER_RUN):
 
     platform = random.choice(platforms)
 
-    # распределяем события по дням
-event_time = datetime.now() - timedelta(
-    days=random.randint(0, DAYS_BACK),
-    hours=random.randint(0, 23),
-    minutes=random.randint(0, 59)
-)
+    # ✅ теперь внутри цикла
+    event_time = datetime.now() - timedelta(
+        days=random.randint(0, DAYS_BACK),
+        hours=random.randint(0, 23),
+        minutes=random.randint(0, 59)
+    )
 
+    # ✅ отступ нормальный
     cur.execute("""
         INSERT INTO raw_events (user_id, event_type, event_time, platform)
         VALUES (%s, %s, %s, %s)
